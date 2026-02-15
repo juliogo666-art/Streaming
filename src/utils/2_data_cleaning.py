e6b51cb9ac2a68bb73c983dc0aabbdfb8f5e5bad
juliogo666-art <julio.go666@gmail.com>
2026-02-15 18:37:43 +0100

    Estructura proyecto

---
+++ src/utils/2_data_cleaning.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+
+def clean_wrong_numerical_types(input_df: pd.DataFrame) -> pd.DataFrame:
+    """
+    parameters
+    ------------
+
+    input_data : pd.dataframe
+    Cambiamos las "," por "." en las columnas numericas
+
+    """
+
+
+def check_duplicated(input_data: pd.DataFrame, id: str) -> pd.DataFrame:
+    """
+    parameters
+    ------------
+
+    input_data : pd.dataframe
+    id : str
+    Comprueba si hay duplicados en el dataframe
+    """

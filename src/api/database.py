e9e9a8436d6100f6874b234ac5635ef3148e2217
Txema <jmtx1995@gmail.com>
2026-03-02 16:49:43 +0100

    datos de .env

--- src/api/database.py
+++ src/api/database.py
@@ -1,12 +1,22 @@
 import mysql.connector
 from mysql.connector import pooling
+import os
+from dotenv import load_dotenv
+
+# Carga las variables del archivo .env
+load_dotenv()
+
+# Accede a ellas
+db_user = os.getenv("DB_USER")
+db_pass = os.getenv("DB_PASSWORD")
 
 # Usamos un "Pool" para manejar múltiples conexiones de forma eficiente
 db_config = {
     "host": "localhost",
-    "user": "eada_new",
-    "password": "Eada2021",
-    "database": "streaming_db"
+    "user": db_user,
+    "password": db_pass,
+    "database": "streaming_db",
+    "port": 3306
 }
 
 conexion_pool = pooling.MySQLConnectionPool(pool_name="mypool", pool_size=5, **db_config)

cf2f32e4ef31464e343b808878ea1d1702352fbc
Txema <jmtx1995@gmail.com>
2026-04-08 14:39:53 +0200

    evitar choque de ids de usuario en mysql y dataset ratings

--- src/data/scripts_sql/modificaciones.sql
+++ src/data/scripts_sql/modificaciones.sql
@@ -6,3 +6,18 @@ SET GLOBAL max_allowed_packet=67108864; -- Esto lo sube a 64MB
 
 #añadir columna video que faltaba en el DDL original
 ALTER TABLE contents ADD COLUMN video BOOLEAN DEFAULT FALSE AFTER title;
+
+#hacer que el id_usuario comience en 500000
+#se ha revisado el dataset de ratings para comprobar el max id_usuario, dejamos margen para mas de 150000 ids que podamos añadir a este dataset
+ALTER TABLE users AUTO_INCREMENT = 500000;
+
+#si ya existen usuarios, reasignar id_usuario para que empiecen en 500000
+SET @nuevo_id := 499999;
+UPDATE users u
+JOIN (
+    SELECT id_usuario, (@nuevo_id := @nuevo_id + 1) AS id_reasignado
+    FROM users
+    ORDER BY id_usuario
+) r ON u.id_usuario = r.id_usuario
+SET u.id_usuario = r.id_reasignado
+where u.id_usuario > 0;
